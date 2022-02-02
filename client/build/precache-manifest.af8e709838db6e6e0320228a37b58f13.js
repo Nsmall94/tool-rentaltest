3c612760b5365ebe792a6342525fb63200975170
Nsmall <nsmall1994@gmail.com>
2022-02-01 19:53:35 -0500

    built

---
+++ client/build/precache-manifest.af8e709838db6e6e0320228a37b58f13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53de14d16504b1fc93da18eaaa66f16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22087f7691f80ba7dc2f",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "f0cd3af5514e2004ee9b",
+    "url": "/static/js/2.21fdc169.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.21fdc169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22087f7691f80ba7dc2f",
+    "url": "/static/js/main.035b47b0.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
